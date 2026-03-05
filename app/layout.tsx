87d9ec9418249f885a1a2f52b59b3350eb70feea
Valentina Marino <valenmarinocol@gmail.com>
2026-03-05 00:36:33 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,6 +59,7 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
             <DesktopNavbar />
             <MobileNavbar />
             <SmoothScroll>{children}</SmoothScroll>
+            <MobileFooter/>
             <WithLove />
           </LandingScrollProvider>
         </AutoThemeProvider>
